cd20b5d155dd041ec8957b10e6677f61a2704165
iputusukmaFEBUI <159942989+iputusukmaFEBUI@users.noreply.github.com>
2024-08-29 22:37:46 +0700

    Update HistoricalYield.py

--- HistoricalYield.py
+++ HistoricalYield.py
@@ -12,10 +12,9 @@ import altair as alt
 st.set_page_config(page_title="Yield Curve Dashboard: Historical and Predictive", page_icon=":bar_chart",layout='wide')
 
 st.header('Historical Yield')
-st.write('by I Putu Sukma Hendrawan')
+st.write('by I Putu Sukma Hendrawan - iputusukma@pm.me')
 
 st.write("This (basic) dashboard provides the historical yield data for Indonesian Government Bonds. The data are collected from investing.com and presented in this dashboard - for both convenience and scientific purposes - using a chart and table. The charts themselves are two-dimensional and three-dimensional line charts. I do hope you enjoy the experience of using this dashboard.")
-st.write("iputusukma@pm.me")
 
 
 #st.sidebar.success("select the dashboard menu")
